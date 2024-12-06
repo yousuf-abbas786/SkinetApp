40ab726a73c235cad9004b233f5d187eb70e47a8
yousuf-abbas786 <yousuf.abbas@hotmail.com>
2024-12-06 04:45:56 +0400

    angular changes

---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, MatBadgeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
